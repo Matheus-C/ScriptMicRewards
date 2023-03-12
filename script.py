f586b1de5b63583a71b92d7ecfc97a971d4baf0c
Matheus Custodio <matheuscustodio@id.uff.br>
2023-03-12 13:17:18 -0300

    adicionado lembrar o endereço e aumentar a espera

--- script.py
+++ script.py
@@ -1,19 +1,29 @@
 import webbrowser
 from time import sleep
-from string import ascii_lowercase
-from random import choice
 from pyautogui import hotkey
+from os.path import exists
 
-
-edge_path = input("coloque aqui o caminho para o executável do edge")
+#verifica se existe o arquivo se sim lê o conteudo do arquivo, senão o cria com o valor inserido pela primeira vez
+if exists("path.txt"):
+    f = open("path.txt", "r")
+    edge_path = f.readline()
+    f.close()
+else:
+    f = open("path.txt", "w")
+    edge_path = input("coloque aqui o caminho para o executável do edge")
+    f.write(edge_path)
+    f.close()
+#numero de segundos para o intervalo, se a internet não estiver muito rápida pode aumentar o intervalo para conseguir carregar a pesquisa
+n = int(input("indique quantos segundos de intervalo entre a abertura de páginas (padrão=1)") or "1")
+input("aperte enter para começar")
 webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edge_path.replace('"', "")))
-
+#url padrão da pesquisa a qual vai diminuindo de 1 em 1 até completar as 45 rotações
 url = "https://www.bing.com/search?q=smdnfbsdjkfgbsdjkfhvasjlfhsdgsdgbaskfhjbasjfhbasklfjb"
 for i in range(45):
     webbrowser.get('edge').open(url)
     url = url[:-1]
-    sleep(1)
+    sleep(n)
     if i != 0:
         hotkey('ctrl', 'w')
-sleep(1)
+sleep(n)
 hotkey('ctrl', 'w')
